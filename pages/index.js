cc24912e2cf56a023966f174e7134bb63dc9c95d
Sean Kerwin <swkerwin@icloud.com>
2022-06-22 22:57:43 +0100

    added tailwind typography and next-themes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
-import Head from "next/head"
-import Image from "next/image"
+import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
     return (
         <div>
             <h1 className="text-red-400">Test</h1>
+            <article className="prose lg:prose-lg">
+                <h1>Garlic bread with cheese: What the science tells us</h1>
+                <p>
+                    For years parents have espoused the health benefits of
+                    eating garlic bread with cheese to their children, with the
+                    food earning such an iconic status in our culture that kids
+                    will often dress up as warm, cheesy loaf for Halloween.
+                </p>
+                <p>
+                    But a recent study shows that the celebrated appetizer may
+                    be linked to a series of rabies cases springing up around
+                    the country.
+                </p>
+            </article>
         </div>
     )
 }
